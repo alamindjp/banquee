f5ba009084c090ce54891255ffde93b58d9d2f95
alamindjp <alaminhossaindjp@gmail.com>
2022-09-30 02:29:25 +0600

    create react app, install packeges, create footer componets

---
+++ src/Pages/Home/Components/Footer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='custom-container border-t'>
+            <div className='flex mt-24'>
+                <div className='w-[550px]'><h4 className='font-bold text-3xl text-primary'>banquee.</h4></div>
+                <footer className="footer p-10 text-base-content gap-20">
+                    <div>
+                        <a className="link link-hover font-bold text-lg">About</a>
+                        <a className="link link-hover text-base text-[#8c8b8e]">Features</a>
+                        <a className="link link-hover text-base text-[#8c8b8e]">Pricing</a>
+                        <a className="link link-hover text-base text-[#8c8b8e]">Support</a>
+                    </div>
+                    <div>
+                        <a className="link link-hover font-bold text-lg">Blog</a>
+                        <a className="link link-hover text-base text-[#8c8b8e]">Products</a>
+                        <a className="link link-hover text-base text-[#8c8b8e]">Technology</a>
+                        <a className="link link-hover text-base text-[#8c8b8e]">Crypto</a>
+                    </div>
+                    <div>
+                        <a className="link link-hover font-bold text-lg">Webflow</a>
+                        <a className="link link-hover text-base text-[#8c8b8e]">Styleguide</a>
+                        <a className="link link-hover text-base text-[#8c8b8e]">Licensing</a>
+                        <a className="link link-hover text-base text-[#8c8b8e]">Changelog</a>
+                    </div>
+                    <div>
+                        <a className="link link-hover font-bold text-lg">Social Media</a>
+                        <a className="link link-hover text-base text-[#8c8b8e]">Twitter</a>
+                        <a className="link link-hover text-base text-[#8c8b8e]">Facebook</a>
+                        <a className="link link-hover text-base text-[#8c8b8e]">Instagram</a>
+                    </div>
+                </footer>
+            </div>
+            <footer className="footer py-4 border-t text-base-content border-base-300 mx-auto justify-between">
+                <p className='flex'>©  Made by <span className='text-primary'>Pawel Gola</span> - Powered by <span className='text-primary'>Webflow</span></p>
+                <p className='flex'>
+                    <span className='mr-8 text-[#8c8b8e]'>Impressum</span>
+                    <span className='text-[#8c8b8e]'>Datenschutz</span>
+                </p>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#5BB5A2",
+          secondary: "#E8F2EE",
+          accent: "#F1DFDF",
+          neutral: "#DFE1F1",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+}
