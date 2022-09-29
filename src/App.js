737751c8425ad48f904d80c50c9105e986932f7d
alamindjp <alaminhossaindjp@gmail.com>
2022-09-30 02:38:22 +0600

    add Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Footer from './Pages/Home/Components/Footer';
+import Header from './Pages/Home/Components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Footer/>
     </div>
   );
